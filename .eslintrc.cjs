878addf1ac8126745531f61445143535da913b28
Kabrax01 <rogalski.arnold@gmail.com>
2024-11-25 16:49:30 +0100

    feat: vitest setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,28 @@
+const { vitest } = require("vitest");
+
 module.exports = {
     root: true,
     env: {
         browser: true,
         es2020: true,
+        "@vitest/env": true,
     },
     extends: [
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
         "plugin:react-hooks/recommended",
+        "plugin:@vitest/legacy-recommended",
     ],
     ignorePatterns: ["dist", ".eslintrc.cjs"],
     parser: "@typescript-eslint/parser",
-    plugins: ["react-refresh", "@typescript-eslint"],
+    plugins: ["react-refresh", "@typescript-eslint", "@vitest"],
     rules: {
         "react-refresh/only-export-components": [
             "warn",
             { allowConstantExport: true },
         ],
+    },
+    globals: {
+        vi: true,
     },
 };
